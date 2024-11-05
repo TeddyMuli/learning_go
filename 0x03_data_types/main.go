bc4c86730763ed3b43db746080b4aabec155e6fa
TeddyMuli <tmuli974@gmail.com>
2024-11-05 21:21:20 +0300

    arrays in go

--- 0x03_data_types/main.go
+++ 0x03_data_types/main.go
@@ -4,13 +4,17 @@ import "fmt"
 
 func main() {
 	// Specifying length
+	var arr0 = [4]int{} // Empty array
 	var arr1 = [3]int{1, 2, 3}
-	arr2 := [3]int{4, 5, 6}
+	arr2 := [5]int{4, 5, 6}
 
 	// Infering the length of the array
 	var arr3 = [...]string{"ted", "muli"}
 	arr4 := [...]string{"goat", "sheep", "cow"}
 
+	arr2[4] = 9
+
+	fmt.Println(arr0)
 	fmt.Println(arr1)
 	fmt.Println(arr2)
 	fmt.Println(arr3)
